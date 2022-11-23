406f153ab1b3f16f80c493efcab447a133c5a285
ki2345 <reddy2002kiran@gmail.com>
2022-11-23 19:01:34 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -15,3 +15,49 @@ function addNewWEField(){
     weOb.insertBefore(newNode, weAddButtonOb);
 
  }
+
+ //generating resume
+ function generateResume(){
+    // console.log("new field");
+    let nameField = document.getElementById('namefield').value;
+    let nameT1 = document.getElementById('nameT1');
+    nameT1.innerHTML = nameField;
+    document.getElementById("nameT2").innerHTML = namefield;
+ }
+
+    //contact
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+    //address
+    document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+    //linkedin
+    document.getElementById("LinkedInfield").innerHTML = document.getElementById("linkedinF").value;
+    //github
+    document.getElementById("GitHubfield").innerHTML = document.getElementById("githubF").value;
+    //twitter
+    document.getElementById("Twitterfield").innerHTML = document.getElementById("twitterF").value;
+    //leetcode
+    document.getElementById("LeetCodefield").innerHTML = document.getElementById("leetcodeF").value;
+    //objective
+    document.getElementById("objectivefield").innerHTML = document.getElementById("objectiveField").value;
+    //work experience
+    let wes = document.getElementsByClassName('weField');
+    let str = "";
+    for(let e of wes){
+        str = str + `<li>${e.value}</li>`;
+    }
+    document.getElementById('wet').innerHTML = str;
+    // academic qualification
+    let aqs = document.getElementsByClassName('edField');
+    let str1 = "";
+    for(let e of aqs){
+        str1 += `<li>${e.value}</li>`;
+    }
+    document.getElementById('aq').innerHTML = str1;
+
+    document.getElementById('resume-form').style.display='none';
+    document.getElementById('resume-template').style.display='block';
+
+    function printResume(){
+        window.print();
+    }
+ 
